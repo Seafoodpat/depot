a745acc1ae5ad9cdf7256cf3a9ec8eba3085cced
Patrick Yip <patrickyip2006@gmail.com>
2016-04-04 22:47:44 +0800

    Smarter Cart

---
+++ db/migrate/20160404140438_combine_items_in_cart.rb
@@ -0,0 +1,29 @@
+class CombineItemsInCart < ActiveRecord::Migration
+  def change
+  	def up
+  		Cart.all.each do |cart|
+  			sums = cart.line_items.group(:product_id).sum(:quantity)
+
+  			sums.each do |product_id, quantity|
+  				if quantity > 1
+  					cart.line_items.where(product_id: product_id).delete_all
+
+  					item = cart.line_items.build(product_id: product_id)
+  					item.quantity = quantity
+  					item.save!
+  				end
+  			end
+  		end
+  	end
+
+    def down
+      LineItem.where("quantity>1").each do |line_items|
+        line_items.quantity.times do
+          LineItem.create cart_id: line_items.cart_id,
+            product_id: line_items.product_id, quantity: 1
+        end
+        line_items.destroy
+      end
+    end
+  end
+end
