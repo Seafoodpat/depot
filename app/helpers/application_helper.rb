fc299113c48fb64141224420aad2dbe74493f72a
Patrick Yip <patrickyip2006@gmail.com>
2016-04-09 13:09:57 +0800

    add ajax feature to cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def hidden_div_if(condition, attributtes = {}, &block)
+		if condition
+			attributtes["style"] = "display: none"
+		end
+		content_tag("div", attributtes, &block)
+	end
 end
